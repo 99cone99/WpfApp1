d61f667a2ea13d7f5d6d844d5f7d9ddd4cb04aa3
Dan Baker <91887184+99cone99@users.noreply.github.com>
2022-11-02 12:46:34 +0000

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (messageText.Text == "")
+            {
+                MessageBox.Show("No message in text Box!", "Warning!");
+
+            }
+            else
+            {
+                string message = messageText.Text;
+                messageText.Text = "";
+                if(localName.Text == "")
+                {
+                    MessageBox.Show("Please enter a name in the Local Name Textbox!", "Warning");
+                    messageText.Text = message;
+                }
+                else
+                {
+                    string name = localName.Text;
+                    chatBox.Text += name + ": " + message + "\n";
+                }
+            }
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            string status = "Server Enabled";
+
+            ThreadStart CheckThread = delegate ()
+            {
+                Dispatcher.Invoke(new Action<string>(SendMessage), status);
+            };
+
+
+            new Thread(CheckThread).Start(); 
+        }
+
+        private void SendMessage(string status)
+        {
+            chatBox.Text += status + "\n";
+        }
+    }
+}
